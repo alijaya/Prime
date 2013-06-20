7cc413dddaaf767fdbad2b379fe7d86c52979fa6
Danny Wilson <danny@onlinetouch.nl>
2013-06-20 23:04:17 +0200

    Bash script to run chxdoc for each Prime library

---
+++ scripts/chxdoc-per-lib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TPLDIR="$1"
+
+set -e
+
+cd "`dirname $0`/.."
+
+for lib in prime*; do
+  ARGS=""
+  for p in cpp cs java js neko php swf; do if [ -f "docs/$lib-$p.xml" ]; then
+    ARGS="$ARGS -f docs/$lib-$p.xml,$p";
+  fi; done;
+
+  if [ -n "$ARGS" ]; then
+    chxdoc -v -o docs/output/api/$lib --generateTodo=true --templatesDir=$TPLDIR/templates/ --deny cpp.* --deny mcover.* $ARGS
+  fi;
+done;
+
+
