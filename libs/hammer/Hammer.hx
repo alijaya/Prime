974d3e4e5ac6505a5a40f10fdbef6884eb92ac8f
Danny Wilson <danny@decube.net>
2014-02-05 13:38:16 +0100

    Improve Hammer.js externs - Fix argument error in untyped js. - Map to proper event class and typedefs

--- libs/hammer/Hammer.hx
+++ libs/hammer/Hammer.hx
@@ -1,59 +1,71 @@
 import js.html.Event;
 import js.html.Element;
 
+@:fakeEnum(String)
+extern enum HammerDirection
+{
+  up;
+  down;
+  left;
+  right;
+}
+
+
 extern class Hammer {
-  public function on(gesture:String, callback:HammerEvent->Void):Element;
+  public function on(gesture:String, callback:GestureEvent->Void):Element;
   static public inline function time(element:Element, ?options : HammerOptions):Hammer {
-    return untyped __js__("Hammer(element, options)");
+    var e = element;
+    var o = options;
+    return untyped __js__("Hammer(e, o)");
   }
-  public function off(gesture:String, callback:HammerEvent->Void):Element;
+  public function off(gesture:String, callback:GestureEvent->Void):Element;
   public function enable(toggle:Bool):Element;
   public function trigger(gesture:String, eventData:Event):Element;
 }
 
 typedef HammerOptions = {
-  var drag: Bool;
-  var drag_block_horizontal: Bool;
-  var drag_block_vertical: Bool;
-  var drag_lock_to_axis: Bool;
-  var drag_max_touches: Int;
-  var drag_min_distance: Int;
-  var hold: Bool;
-  var hold_threshold: Int;
-  var hold_timeout: Int;
-  var prevent_default: Bool;
-  var prevent_mouseevents: Bool;
-  var release: Bool;
-  var show_touches: Bool;
-  var stop_browser_behavior: {
-    userSelect: String,
-    touchAction: String,
-    touchCallout: String,
-    contentZooming: String,
-    userDrag: String,
-    tapHighlightColor: String
-  };
-  var swipe: Bool;
-  var swipe_max_touches: Int;
-  var swipe_velocity: Float;
-  var tap: Bool;
-  var tap_always: Bool;
-  var tap_max_distance: Int;
-  var tap_max_touchtime: Int;
-  var doubletap_distance: Int;
-  var doubletap_interval: Int;
-  var touch: Bool;
-  var transform: Bool;
-  var transform_always_block: Bool;
-  var transform_min_rotation: Int;
-  var transform_min_scale: Float;
+  ?drag: Bool,
+  ?drag_block_horizontal: Bool,
+  ?drag_block_vertical: Bool,
+  ?drag_lock_to_axis: Bool,
+  ?drag_max_touches: Int,
+  ?drag_min_distance: Int,
+  ?hold: Bool,
+  ?hold_threshold: Int,
+  ?hold_timeout: Int,
+  ?prevent_default: Bool,
+  ?prevent_mouseevents: Bool,
+  ?release: Bool,
+  ?show_touches: Bool,
+  ?stop_browser_behavior: {
+    ?userSelect: String,
+    ?touchAction: String,
+    ?touchCallout: String,
+    ?contentZooming: String,
+    ?userDrag: String,
+    ?tapHighlightColor: String
+  },
+  ?swipe: Bool,
+  ?swipe_max_touches: Int,
+  ?swipe_velocity: Float,
+  ?tap: Bool,
+  ?tap_always: Bool,
+  ?tap_max_distance: Int,
+  ?tap_max_touchtime: Int,
+  ?doubletap_distance: Int,
+  ?doubletap_interval: Int,
+  ?touch: Bool,
+  ?transform: Bool,
+  ?transform_always_block: Bool,
+  ?transform_min_rotation: Int,
+  ?transform_min_scale: Float,
 }
 
 typedef HammerTouch = {
   identifier:Int, 
   pageX:Int, 
   pageY:Int, 
-  target:String
+  target:Element
 }
 
 typedef Center = {
@@ -61,57 +73,58 @@ typedef Center = {
   pageY: Int
 }
 
-typedef StartEvent = {
-  var center: Center;
-  var eventType: String;
+typedef HammerEvent = {
+  /** center position of the touches. contains pageX and pageY */
+  var center:      Center;
+  /** matches Hammer.EVENT_START|MOVE|END */
+  var eventType:   String;
+  /** kind of pointer that was used. matches Hammer.POINTER_MOUSE|TOUCH */
   var pointerType: String;
-  var srcEvent: Event;
-  var target: String;
-  var timestamp: Int;
-  var touches: Array<HammerTouch>;
+  /** the source event, like TouchStart or MouseDown * */
+  var srcEvent:    Event;
+  /** A reference to the target to which the event was originally dispatched. */
+  var target(default,null) : js.html.EventTarget;
+  /** The time that the event was created. */
+  var timeStamp(default,null) : Int;
+  /** touches (fingers; mouse) on the screen */
+  var touches:     Array<HammerTouch>;
 
   function preventDefault():Void;
   function stopDetect():Void;
   function stopPropagation():Void;
 }
 
-typedef HammerEvent = {
-  /** time the event occurred */
-  var timestamp:   Int;
-  /** target element */
-  var target:      String;
-  /** touches (fingers; mouse) on the screen */
-  var touches:     Array<HammerTouch>;
-  /** kind of pointer that was used. matches Hammer.POINTER_MOUSE|TOUCH */
-  var pointerType: String;
-  /** center position of the touches. contains pageX and pageY */
-  var center:      Center;
+extern class GestureEvent extends Event
+{
+  var gesture: HammerGesture;
+}
+
+typedef HammerGesture = {>HammerEvent,
   /** the total time of the touches in the screen */
   var deltaTime:   Int;
   /** the delta on x axis we haved moved */
-  var deltaX:      Int;
+  var deltaX:      Float;
   /** the delta on y axis we haved moved */
-  var deltaY:      Int;
+  var deltaY:      Float;
   /** the velocity on the x */
-  var velocityX:   Int;
+  var velocityX:   Float;
   /** the velocity on y */
-  var velocityY:   Int;
+  var velocityY:   Float;
   /** the angle we are moving */
-  var angle:       Int;
-  /** the direction we are moving. matches Hammer.DIRECTION_UP|DOWN|LEFT|RIGHT */
-  var direction:   String;
+  var angle:       Float;
+  /** the direction we are moving */
+  var direction:   HammerDirection;
   /** the distance we haved moved */
-  var distance:    Int;
+  var distance:    Float;
   /** scaling of the touches, needs 2 touches */
-  var scale:       Int;
+  var scale:       Float;
   /** rotation of the touches, needs 2 touches * */
-  var rotation:    Int;
-  /** matches Hammer.EVENT_START|MOVE|END */
-  var eventType:   String;
-  /** the source event, like TouchStart or MouseDown * */
-  var srcEvent:    Event;
+  var rotation:    Float;
   /** contains the same properties as above,
   but from the first touch. this is used to calculate
   distances, deltaTime, scaling etc */
-  var startEvent:  StartEvent;
+  var startEvent:  HammerEvent;
+
+  var interimAngle: Float;
+  var interimDirection: HammerDirection;
 }
